70be649c102c907b1ef1ebe64f7645f3a675a12b
alfiansaputra01 <100255999+alfiansaputra01@users.noreply.github.com>
2022-05-24 14:50:16 +0700

    Add files via upload

--- src/control/movieControl.java
+++ src/control/movieControl.java
@@ -1,59 +1,111 @@
 package control;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import model.movieModel;
-import view.FormMovie;
-
 import java.sql.ResultSet;
 import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
 import model.movieModel;
-import view.FormMovie;
+import view.MovieView;
 
 public class movieControl {
-    private FormMovie view;
-    private movieModel model;
+   // private FormMovie view;
+    movieModel model;
+    MovieView view;
+    String data;
 
-    private String judul;
-    double alur, penokohan, akting, nilai;
-
-    public movieControl(FormMovie view){
+    public movieControl(movieModel model,MovieView view) {
+        this.model = model;
         this.view = view;
-        model = new movieModel();
-    }
+        
+        if (model.getBanyakData()!=0) {
+            String dataMovie[][] = model.readMovie();
+            view.tabel.setModel((new JTable(dataMovie, view.namaKolom)).getModel());
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "Data Tidak Ada");
+        }
+        
+        view.btnTambah.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                String judul = view.getJudul();
+                double alur = Double.parseDouble(view.getAlur());
+                double penokohan = Double.parseDouble(view.getPenokohan());
+                double akting = Double.parseDouble(view.getAkting());
+                model.createMovie(judul,alur,penokohan,akting);
+         
+                String dataMovie[][] = model.readMovie();
+                view.tabel.setModel((new JTable(dataMovie, view.namaKolom)).getModel());
+            }
+        });
+        
+        view.btnUpdate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                String judul = view.getJudul();
+                double alur = Double.parseDouble(view.getAlur());
+                double penokohan = Double.parseDouble(view.getPenokohan());
+                double akting = Double.parseDouble(view.getAkting());
+                model.updateMovie(judul,alur,penokohan,akting);
+                
+                String dataMovie[][] = model.readMovie();
+                view.tabel.setModel((new JTable(dataMovie, view.namaKolom)).getModel());
+            }
+        });
+        
+  
+        
+        view.btnDelete.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+               int input = JOptionPane.showConfirmDialog(null,
+                "Apa anda ingin menghapus film ?", "Pilih Opsi...", JOptionPane.YES_NO_OPTION);
 
-    public  ResultSet tampilMovie(){
-        return model.tampilMovie();
-    }
-
-   public void inputMovie(String judul, double alur, double penokohan, double akting, double nilai){
-       model.setJudul(judul);
-       model.setAlur(alur);
-       model.setPenokohan(penokohan);
-       model.setAkting(akting);
-       model.setNilai(nilai);
-      
-   }
+            if (input == 0) {
+                model.deleteMovie(data);
+                String dataMovie[][] = model.readMovie();
+                view.tabel.setModel((new JTable(dataMovie, view.namaKolom)).getModel());
+            } else {
+                JOptionPane.showMessageDialog(null, "Tidak Jadi Dihapus");
+            }
+                }
+        });
+        
+        view.btnReset.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                
+                view.tfJudul.setText("");
+                view.tfAlur.setText("");
+                view.tfPenokohan.setText("");
+                view.tfAkting.setText("");
+            }
+        });
+        
     
-    public boolean insertMovie(){
-        return model.insertMovie();
+          view.tabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mousePressed(e);
+                
+                int row = view.tabel.getSelectedRow();
+                data = view.tabel.getValueAt(row, 0).toString();
+                String dataUpdate[] = new String[4];
+                dataUpdate[0] = view.tabel.getValueAt(row, 0).toString();
+                dataUpdate[1] = view.tabel.getValueAt(row, 1).toString();
+                
+                
+                System.out.println(data);
+                 
+                
+                
+            }
+           });
     }
     
-    public void hapusMovie(String judul){
-        model.setJudul(judul);
-    }
-    public boolean deleteMovie(){
-        return model.deleteMovie();
-    }
-    
-    public void ubahMovie(String judul, double alur, double penokohan, double akting, double nilai){
-       model.setJudul(judul);
-       model.setAlur(alur);
-       model.setPenokohan(penokohan);
-       model.setAkting(akting);
-       model.setNilai(nilai);
-    }
-    public boolean updateMovie(){
-        return model.updateMovie();
-    }
-
-
 }
