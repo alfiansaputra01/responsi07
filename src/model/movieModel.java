a7903123aa4cfe577f82619a511d365e7ab59d5e
alfiansaputra01 <100255999+alfiansaputra01@users.noreply.github.com>
2022-05-24 14:51:05 +0700

    Add files via upload

--- src/model/movieModel.java
+++ src/model/movieModel.java
@@ -1,94 +1,138 @@
-
 package model;
 
-import koneksi.koneksiDB;
-import java.sql.ResultSet;
+import Util.Connector;
+import javax.swing.JOptionPane;
+import java.sql.*;
 
-
+/**
+ *
+ * @author Lenovo
+ */
 public class movieModel {
-
-    private final koneksiDB koneksi;
-    private ResultSet rsMovie;
-    private String query;
-    private boolean status;
+    
+    protected String moviename;
+    protected double alur,penokohan,akting,nilai;
+    
+    Connector con;
+    Statement stmt;
     
     public movieModel(){
-        koneksi = new koneksiDB();
-        koneksi.getConn();
+    
+        con = new Connector();
+
     }
     
-    private String judul;
-    double alur, penokohan, akting, nilai;
+    public void createMovie(String nama, double alur, double penokohan,double akting){
+       
+       try{
+           nilai = (int) ((alur + penokohan + akting)/3);
+           stmt = (Statement) con.getConn().createStatement();
+           String sql = "INSERT INTO movie VALUES('" +nama+ "',"+alur+","+penokohan+","+akting+","+ nilai +")";     
+           stmt.executeUpdate(sql);
+           JOptionPane.showMessageDialog(null, "input berhasil");
+           
+       }
+       catch(SQLException e){
+           JOptionPane.showMessageDialog(null, e.getMessage());
+       }
+    }
     
-     public double getAlur() {
-        return alur;
+    public String[][] readMovie(){   
+        try{
+            
+            int jmlData = 0;
+           
+           stmt = (Statement) con.getConn().createStatement();
+            
+            String data[][] = new String[getBanyakData()][5]; //baris, kolom nya ada 4
+            
+            String query = "select * from movie"; 
+            ResultSet resultSet = stmt.executeQuery(query);
+            
+           
+            while (resultSet.next()){
+                data[jmlData][0] = resultSet.getString("judul"); //harus sesuai nama kolom di mysql
+                data[jmlData][1] = String.valueOf(resultSet.getDouble("alur"));                
+                data[jmlData][2] = String.valueOf(resultSet.getDouble("penokohan"));
+                data[jmlData][3] = String.valueOf(resultSet.getDouble("akting"));
+                data[jmlData][4] = String.valueOf(resultSet.getDouble("nilai"));
+                jmlData++;
+            }
+            
+//             System.out.println(data[jmlData][1]);
+            
+            JOptionPane.showMessageDialog(null, "baca berhasil");
+            return data;
+       }
+       catch(SQLException e){
+           JOptionPane.showMessageDialog(null, "baca gagal");
+           return null;
+       }
     }
-
-    public void setAlur(double alur) {
-        this.alur = alur;
-    }
-
-    public double getPenokohan() {
-        return penokohan;
-    }
-
-    public void setPenokohan(double penokohan) {
-        this.penokohan = penokohan;
-    }
-
-    public double getAkting() {
-        return akting;
-    }
-
-    public void setAkting(double akting) {
-        this.akting = akting;
-    }
-
-    public double getNilai() {
-        return nilai;
-    }
-
-    public void setNilai(double nilai) {
-        this.nilai = nilai;
-    }
-
-    public String getJudul() {
-        return judul;
-    }
-
-    public void setJudul(String judul) {
-        this.judul = judul;
-    }
-
-     
     
-    public ResultSet tampilMovie(){
-        query = "SELECT * FROM movie_db ";
-        status = koneksi.eksekusiQuery(query, true);
-        if(status){
-            rsMovie = koneksi.getRs();
+    public void updateMovie(String nama, double alur, double penokohan,double akting){
+        
+        try{
+            int jmlData = 0;
+            
+            nilai = (int) ((alur + penokohan + akting)/3);
+           
+            stmt = (Statement) con.getConn().createStatement();
+            
+            String query1 = "select * from movie where judul = '" + nama + "'";
+            
+            ResultSet res = stmt.executeQuery(query1);
+            
+            while(res.next()){
+                jmlData++;
+            }
+          
+            if (jmlData == 0) {
+                JOptionPane.showMessageDialog(null, "Data tidak ditemukan");
+            }
+            
+            else{
+                
+                String query = "UPDATE movie SET alur='" + alur + "', penokohan='" + penokohan + "',akting = '" + akting + "',nilai='"+ nilai+"' WHERE judul= '" + nama + "'" ;  
+                stmt.executeUpdate(query);
+                JOptionPane.showMessageDialog(null, "update berhasil");
+            }
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
         }
-        return rsMovie;
-    }
-    public boolean insertMovie(){
-        query = "insert into movie_db (judul, alur, penokohan, akting, nilai) values ('"+judul+"','"+alur+"','"+penokohan+"','"+akting+"','"+nilai+"')";
-        status= koneksi.eksekusiQuery(query, false);
-        return status;
-    }
-   
-    public boolean deleteMovie(){
-        query = "delete from movie_db where judul='"+judul+"'";
-        status= koneksi.eksekusiQuery(query, false);
-        return status;
+    
     }
     
-    public boolean updateMovie(){
-        query = "update movie_db set alur='"+alur+"',penokohan='"+penokohan+"',akting='"+akting+"',nilai='"+nilai+"' where judul='"+judul+"'";
-        status= koneksi.eksekusiQuery(query, false);
-        return status;
+    
+    
+   
+    public int getBanyakData(){
+        int jmlData = 0;
+        try{
+            stmt = con.getConn().createStatement();
+            String query = "Select * from movie";
+            ResultSet resultSet = stmt.executeQuery(query);
+            while (resultSet.next()){ 
+                jmlData++;
+            }
+            return jmlData;
+            
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+            System.out.println("SQL Error");
+            return 0;
+        }
     }
-
-   
-   
+    
+    public void deleteMovie (String nama) {
+        try{
+            String query = "DELETE FROM movie WHERE judul = '"+nama+"'";
+            stmt = con.getConn().createStatement();
+            stmt.executeUpdate(query);
+            JOptionPane.showMessageDialog(null, "Berhasil Dihapus");
+            
+        }catch(SQLException sql) {
+            System.out.println(sql.getMessage());
+        }
+    }
 }
-
